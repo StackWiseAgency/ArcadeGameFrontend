4b700f05c50bf4e4b80e0701bb83b47c71ce4eb6
StackWiseAgency <stackwiseagency@gmail.com>
2025-02-16 00:38:28 +0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom"; // ✅ Import BrowserRouter
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
+  );
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });

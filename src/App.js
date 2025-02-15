9f1eca48826ceb550d784c55e2b2a480f8241894
StackWiseAgency <stackwiseagency@gmail.com>
2025-02-16 02:36:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import SigninPage from "./pages/SigninPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SigninPage from "./pages/SigninPage"; 
 import SignupPage from "./pages/SignupPage";
 import GuestSignin from "./pages/GuestSignin";
 import GameSelectionPage from "./pages/GameSelectionPage";
@@ -14,23 +14,17 @@ function App() {
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/guest" element={<GuestSignin />} />
         <Route path="/game-select" element={<GameSelectionPage />} />
-
-        {/* Catch-all 404 Route */}
+        
+        {/* Fallback for unmatched routes */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
 }
 
-// Custom 404 Page
+// Optional NotFound Component
 function NotFound() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h2>404 - Page Not Found</h2>
-      <p>The page you are looking for does not exist.</p>
-      <a href="/">Go Back to Home</a>
-    </div>
-  );
+  return <h2 style={{ textAlign: "center" }}>404 - Page Not Found</h2>;
 }
 
-export default App;
+export default App; 

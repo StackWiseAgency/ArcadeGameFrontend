7a223ede7eb73b119ffc887f9d8af8a42fbca7b6
StackWiseAgency <stackwiseagency@gmail.com>
2025-02-16 01:28:30 +0500

    Delete src/App.test.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders welcome message', () => {
-  // Update this line to match the actual text in your component
-  const headingElement = screen.getByText(/Welcome to Fling Disc/i);
-  expect(headingElement).toBeInTheDocument();
-});
